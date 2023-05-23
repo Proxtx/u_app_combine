bfd1dc1c24b5700c2db740c44332b8fd95aeb471
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-23 18:30:58 +0200

    Error handling

--- main.js
+++ main.js
@@ -10,62 +10,71 @@ export class App {
   }
 
   async init() {
-    await this.findClient();
-    let modules = JSON.parse(
-      (
-        await this.client.request("http", "request", [
-          "POST",
-          this.config.url,
-          JSON.stringify({
-            info: true,
-          }),
-          "application/json",
-        ])
-      ).result.response
-    ).modules.map((v) => v.name);
-
-    for (let module of modules) {
-      this.modules[module] = JSON.parse(
+    try {
+      await this.findClient();
+      let modules = JSON.parse(
         (
           await this.client.request("http", "request", [
             "POST",
             this.config.url,
             JSON.stringify({
               info: true,
-              module,
             }),
             "application/json",
           ])
         ).result.response
-      ).exports;
-    }
+      ).modules.map((v) => v.name);
 
-    for (let module in this.modules) {
-      for (let exportName in this.modules[module]) {
-        this.definitions.methods[module + exportName] = {
-          name: exportName,
-          arguments: this.config.argumentConfig[module]?.[exportName]
-            ? this.config.argumentConfig[module][exportName]
-            : [],
-        };
-        this[module + exportName] = async function () {
-          return JSON.parse(
-            (
-              await this.client.request("http", "request", [
-                "POST",
-                this.config.url,
-                JSON.stringify({
-                  module,
-                  export: exportName,
-                  arguments: [...arguments],
-                }),
-                "application/json",
-              ])
-            ).result.response
-          ).data;
-        };
+      for (let module of modules) {
+        this.modules[module] = JSON.parse(
+          (
+            await this.client.request("http", "request", [
+              "POST",
+              this.config.url,
+              JSON.stringify({
+                info: true,
+                module,
+              }),
+              "application/json",
+            ])
+          ).result.response
+        ).exports;
       }
+
+      for (let module in this.modules) {
+        for (let exportName in this.modules[module]) {
+          this.definitions.methods[module + exportName] = {
+            name: exportName,
+            arguments: this.config.argumentConfig[module]?.[exportName]
+              ? this.config.argumentConfig[module][exportName]
+              : [],
+          };
+          this[module + exportName] = async function () {
+            return JSON.parse(
+              (
+                await this.client.request("http", "request", [
+                  "POST",
+                  this.config.url,
+                  JSON.stringify({
+                    module,
+                    export: exportName,
+                    arguments: [...arguments],
+                  }),
+                  "application/json",
+                ])
+              ).result.response
+            ).data;
+          };
+        }
+      }
+    } catch {
+      this.refreshCombine = true;
+      console.log("Combine failed!");
     }
+  }
+
+  async updateDefinitions() {
+    await this.init();
   }
 
   async findClient() {
